23b04bb3a93564529ee30ce4d8104aa0fbd27f30
David Farrell <shokku.ra@gmail.com>
2019-06-22 14:43:02 +0100

    Move APU code into inc file

--- src/square.s
+++ src/square.s
@@ -1,33 +1,19 @@
 .include "header/ines.inc"
+.include "apu.inc"
 
 .segment "STARTUP"
 
 .segment "CODE"
 
+apu: .tag APU
+
 reset:
   sei                                                                           ; disable IRQs
   cld                                                                           ; disable decimal mode
 
-  lda #%00000001                                                                ; enable pulse channel 1
-  sta $4015                                                                     ; $4015 = APU: status
-                                                                                ; [0:0] enable pulse channel 1
-                                                                                ; [1:1] enable pulse channel 2
-                                                                                ; [2:2] enable triangle channel
-                                                                                ; [3:3] enable noise channel
-                                                                                ; [4:4] enable delta modulation channel
-                                                                                ; [5:7] reserved
-  lda #$e3                                                                      ; middle C = 440 Hz = ~2.27ms = 0xe3
-  sta $4002                                                                     ; $4002 = APU: bits 0:7 of pulse 1 timer
-  lda #$0                                                                       ; set high bits of timer
-  sta $4003                                                                     ; $4003 = APU: length counter load and bits 8:10 of pulse 1 timer
-                                                                                ; [0:2] bits 8:10 of timer
-                                                                                ; [3:7] length counter load
-  lda #%10111111                                                                ; set pulse options
-  sta $4000                                                                     ; $4000 = APU: pulse 1 options
-                                                                                ; [0:3] volume/envelope divider period
-                                                                                ; [4:4] constant volume/envelope
-                                                                                ; [5:5] length counter halt
-                                                                                ; [6:7] duty cycle
+  ldx apu
+  ldy #$e3                                                                      ; middle C = 440 Hz = ~2.27ms = 0xe3
+  jsr pulse1_play
 hang:
   jmp hang
 nmi:
